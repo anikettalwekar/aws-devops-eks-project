89ec7f0b2731bde843a302445ecd74878255c3ce
anikettalwekar <talwekaraniket.11@gmail.com>
2025-10-21 21:03:11 +0530

    Create variables.tf

---
+++ infra/terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+  default     = "demo-eks"
+}
+
+variable "ecr_repo_name" {
+  description = "ECR repository name"
+  type        = string
+  default     = "myapp"
+}
+
+variable "desired_size" {
+  description = "Number of worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "instance_types" {
+  description = "EC2 instance types for node group"
+  type        = list(string)
+  default     = ["t3.medium"]
+}
